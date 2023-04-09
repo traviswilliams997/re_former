0f3501f69ba423913b9ae45401d7c59aa95f3796
Travis Williams <35243732+traviswilliams997@users.noreply.github.com>
2023-04-09 03:02:46 -0500

    Add edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-
+    def show 
+        @user = User.find(params[:id])
+    end
     def new 
         @user =User.new
     end
@@ -14,6 +16,21 @@ class UsersController < ApplicationController
           render :new, status: :unprocessable_entity
         end
     end
+
+    def edit 
+        @user = User.find(params[:id])
+
+    end
+
+    def update
+        @user = User.find(params[:id])
+    
+        if @user.update(allowed_user_params)
+          redirect_to @user
+        else
+          render :edit, status: :unprocessable_entity
+        end
+      end
 private 
 def allowed_user_params
     params.require(:user).permit(:username,:email,:password)
